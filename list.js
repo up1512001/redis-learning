79cb8bbabaeb61d7a2d9b00633475dd6419b3fb5
Utsav <utsav.patel1512@outlook.com>
2023-09-25 23:22:50 +0530

    keys and del

--- list.js
+++ list.js
@@ -4,15 +4,25 @@ async function init(){
     // list can be use as stack or queue
     // L -> Insert & L -> Pop => STACK
     // L -> Insert & R -> Pop => QUEUE
-    // await client.lpush("hey","bbgsdsrezteaesssssssssss");
+    // await client.lpush("hey","bbgsdsrezteaes");
     // await client.rpush("hey","last element");
-    console.log(await client.lpop("hey"));
-    console.log(await client.rpop("hey"));
+    // console.log(await client.lpop("hey"));
+    // console.log(await client.rpop("hey"));
     console.log(await client.llen("hey"));
     // wait for 10 s or until element get enter`
-    console.log(await client.blpop("hey",10));
+    // console.log(await client.blpop("hey",10));
     console.log("finished");
+    
+    // console.log(await client.lrange("hey",0,2));    
     // await client.lpush("hey","utsav");
+
+    // delete item => 0 or 1
+    console.log(await client.del("hey_lpush"));
+
+    // keys
+    console.log(await client.keys("user:*"));
+
+
 }
 
 init();
