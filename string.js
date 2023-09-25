615276b704a90c40dcd5667cda13adffdcb27bce
Utsav <utsav.patel1512@outlook.com>
2023-09-25 22:51:58 +0530

    string get set expire

--- string.js
+++ string.js
@@ -1,7 +1,9 @@
 const client = require('./client');
 
 async function init(){
-    const result = await client.get("user1");
+    await client.set("user:4","hy from nodejs");
+    await client.expire("user:4",100); // after 100 s remove from redis data
+    const result = await client.get("user:4");
     console.log(result);
 }
 
