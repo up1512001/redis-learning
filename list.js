11d33e2e2ac422412ebb4cbdca5c46aa4b3db0c5
Utsav <utsav.patel1512@outlook.com>
2023-09-25 23:12:07 +0530

    list blpop (block mode)

--- list.js
+++ list.js
@@ -4,10 +4,15 @@ async function init(){
     // list can be use as stack or queue
     // L -> Insert & L -> Pop => STACK
     // L -> Insert & R -> Pop => QUEUE
-    await client.lpush("hey","bbgsdsrezteaesssssssssss");
-    await client.rpush("hey","last element");
+    // await client.lpush("hey","bbgsdsrezteaesssssssssss");
+    // await client.rpush("hey","last element");
+    console.log(await client.lpop("hey"));
+    console.log(await client.rpop("hey"));
+    console.log(await client.llen("hey"));
+    // wait for 10 s or until element get enter`
+    console.log(await client.blpop("hey",10));
     console.log("finished");
-
+    // await client.lpush("hey","utsav");
 }
 
 init();
